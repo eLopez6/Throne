0990c5897f19ad8f3bc633467af9e8647246283b
Emilio Lopez <eil11@case.edu>
2018-02-17 19:19:14 -0500

    Begin driver writing - nothing special here

--- src/driver/SlideshowDriver.java
+++ src/driver/SlideshowDriver.java
@@ -1,7 +1,28 @@
 package driver;
 
+import driver.ConfigurationManager.RequiredField;
+
 /**
  * This class will handle the other classes in the project
  */
 public class SlideshowDriver {
+
+    public static void main (String[] args)
+    throws Exception {
+
+        String propertiesPath = "../../Throne.properties";
+
+        if (args.length == 1) {
+            propertiesPath = args[0];
+        }
+
+        ConfigurationManager config = ConfigurationManager.Builder.build(propertiesPath);
+        ImageAggregator aggregator = ImageAggregator.Builder.build(
+                config.getValuesForPropertyField(RequiredField.EXTENSIONS),
+                config.getValuesForPropertyField(RequiredField.DIRECTORIES));
+
+    }
+
+
+
 }
