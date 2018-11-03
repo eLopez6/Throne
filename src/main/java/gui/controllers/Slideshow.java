4b013616e226b9c2c655ab3381757b1360544486
Emilio Lopez <eil11@case.edu>
2018-11-03 00:03:23 -0400

    Start slideshow controller non-sense

--- src/main/java/gui/controllers/Slideshow.java
+++ src/main/java/gui/controllers/Slideshow.java
@@ -1,4 +1,17 @@
 package gui.controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
 public class Slideshow {
+    @FXML
+    private ImageView img;
+
+
+    public void nextImage(String path) {
+        img.setImage(new Image(path));
+    }
+
+
 }
